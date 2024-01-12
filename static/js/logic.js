c54a5d20fe5d0214d50cfe704d13ca9572ff604f
Jesse R Reeves <143226100+JesseRReeves@users.noreply.github.com>
2024-01-12 12:33:28 -0800

    final update

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,6 +17,22 @@ basemap.addTo(map);
 // Retrieve the earthquake data from the geojson
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
+    function markerColor(depth) {
+        if (depth > 90) {
+            return "#ea2c2c"
+        } else if (depth > 70) {
+            return "#ea822c"
+        } else if (depth > 50) {
+            return "#ee9c00"
+        } else if (depth > 30) {
+            return "#eecc00"
+        } else if (depth > 10) {
+            return "#d4ee00"
+        } else {
+            return "#98ee00"
+        }
+    }
+
     function markerRadius(magnitude) {
         if (magnitude === 0) {
             return 1;
@@ -27,7 +43,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     function styleInfo(feature) {
         return {
             fillOpacity: 0.9,
-            radius: markerRadius(feature.properties.mag)
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            radius: markerRadius(feature.properties.mag),
+            weight: 0.5
         }
     }
 
@@ -44,4 +62,26 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             );
         }
     }).addTo(map);
+
+    // Create a legend
+
+    var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90],
+        colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
 })
