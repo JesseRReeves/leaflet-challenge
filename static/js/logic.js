fde36a1ef86878c1e1ea388bd9df9c2c099defcf
Jesse R Reeves <143226100+JesseRReeves@users.noreply.github.com>
2024-01-12 11:37:34 -0800

    logic.js push

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,9 +16,32 @@ basemap.addTo(map);
 
 // Retrieve the earthquake data from the geojson
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+
+    function markerRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return 4 * magnitude
+    }
+
+    function styleInfo(feature) {
+        return {
+            fillOpacity: 0.9,
+            radius: markerRadius(feature.properties.mag)
+        }
+    }
+
     L.geoJSON(data, {
-        pointToLayer: function (CSSFontFeatureValuesRule, latlng) {
+        pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "Magnitude: " + feature.properties.mag +
+                "<br>Depth: " + feature.geometry.coordinates[2] +
+                "<br>Location: " + feature.properties.place
+            );
         }
     }).addTo(map);
 })
